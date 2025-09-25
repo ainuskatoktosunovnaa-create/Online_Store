ffa6ccacf3eb1b0f6df0158250d9d782d60f2cd7
Ainuska <ainuskatoktosunovnaa@gmail.com>
2025-09-25 12:50:16 +0300

    add post recvers

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import ProductCard from "./components/ProguctCard";
 
 function App() {
   const [products, setProducts] = useState([]);
-const API = "https://68d269b5cc7017eec543c2b5.mockapi.io/api/v1/products";
+  const API = "https://68d269b5cc7017eec543c2b5.mockapi.io/api/v1/products";
+  const [newProduct, setNewProduct] = useState({
+    title: "",
+    price: "",
+    img: "",
+    description: ""
+  });
 
   async function getProducts() {
     const { data } = await axios.get(API);
@@ -24,29 +30,96 @@ const API = "https://68d269b5cc7017eec543c2b5.mockapi.io/api/v1/products";
     //     getProducts();
     //   }
     // }
-     
-    const respons = await axios.delete(`${API}/${id}`)
+
+    const respons = await axios.delete(`${API}/${id}`);
     console.log(respons);
-    
-    if(respons.status == 200){
-      getProducts()
-      
+
+    if (respons.status == 200) {
+      getProducts();
     }
   }
+console.log(newProduct);
+
+ async function addProduct(){
+  if(newProduct.title.trim && newProduct.price && newProduct.img.trim && newProduct.description){
+    const res = await axios.post(API, newProduct);
+    console.log(res);
+    if (res.status == 200) {
+      setNewProduct({
+        title: "",
+        price: "",
+        img: "",
+        description: "",
+      });
+      getProducts();
+    }
+  }
+  const res = await axios.post(API,newProduct)
+  console.log(res);
+  if(res.status == 200){
+    setNewProduct({
+      title: "",
+      price: "",
+      img: "",
+      description: ""
+  })
+    getProducts()
+  }
+  
+}
 
   useEffect(() => {
     getProducts();
   }, []);
 
-
-
   return (
     <div>
-      {products.map((obj, index) => (
-        <ProductCard product={obj} key={index} deleteProduct={deleteProduct}/>
-        
-      ))}
-   </div>
+      <div className="form">
+        <h1>Add new Product</h1>
+
+        <input
+          type="text"
+          placeholder="Product Title"
+          value={newProduct.title}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, title: e.target.value })
+          }
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={newProduct.price}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, price: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Product Image"
+          value={newProduct.img}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, img: e.target.value })
+          }
+        />
+        <textarea
+          type="text"
+          placeholder="Product Description"
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, image: e.target.value })
+          }
+        ></textarea>
+        <button onClick={addProduct}>Add Product</button>
+      </div>
+      <div className="link">
+        {products.map((obj, index) => (
+          <ProductCard
+            product={obj}
+            key={index}
+            deleteProduct={deleteProduct}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/ProguctCard/index.jsx
+++ src/components/ProguctCard/index.jsx
@@ -1,22 +1,17 @@
- import "./style.css"
+import "./style.css";
 
-function ProductCard({product, index, deleteProduct}){
-  
-
-    return (
-
-        <div className="link">
-        <div className="card">
-          {/* <img src={product.img} alt="" /> */}
-          <h3 key={index}>{product.title}</h3>
-          <button>{product.price}</button>
-          <h4>{product.description}</h4>
-          <div >
-            <button >Edit</button>
-            <button  onClick={() => deleteProduct(product.id)} >Delete</button>
-          </div>
-        </div>
+function ProductCard({ product, index, deleteProduct }) {
+  return (
+    <div className="card">
+      {/* <img src={product.img} alt="" />  */}
+      <h3 key={index}>{product.title}</h3>
+      <button>{product.price}</button>
+      <h4>{product.description}</h4>
+      <div>
+        <button>Edit</button>
+        <button onClick={() => deleteProduct(product.id)}>Delete</button>
       </div>
-    );
+    </div>
+  );
 }
-export default ProductCard
+export default ProductCard;
